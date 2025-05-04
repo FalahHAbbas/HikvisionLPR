d1b26e4a7b4d67752e8e06778afeb8c1e82febb5
falah <FalahH.Abbas@gmail.com>
2025-05-04 18:06:23 +0300

    handle errors

--- src/main/java/com/falah/PrintingService.java
+++ src/main/java/com/falah/PrintingService.java
@@ -3,6 +3,9 @@ package com.falah;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.awt.print.PageFormat;
+import java.awt.print.Paper;
+import java.awt.print.PrinterJob;
 import java.io.File;
 import java.net.URL;
 
@@ -15,18 +18,8 @@ public class PrintingService {
     public void initPrinter(String imageUrl, String text) {
         try {
             URL url = new URL(imageUrl);
-            BufferedImage image = ImageIO.read(url);
-
-            if (text != null) {
-                Graphics2D g = image.createGraphics();
-                g.setFont(new Font("Arial", Font.BOLD, 20));
-                g.setColor(Color.BLACK);
-                int centerX = image.getWidth() / 2;
-                int centerY = image.getHeight() - 10;
-
-                g.drawString(text, centerX - (g.getFontMetrics().stringWidth(text) / 2), centerY);
-                g.dispose();
-            }
+            BufferedImage image = ImageIO.read(url);;
+            BufferedImage imageP = ImageIO.read(new File("image.png"));
             File outputfile = new File("image.png");
             ImageIO.write(image, "png", outputfile);
             Runtime.getRuntime().exec("lp -o fit-to-page " + outputfile.getAbsolutePath());
--- src/main/java/com/falah/RampService.java
+++ src/main/java/com/falah/RampService.java
@@ -6,82 +6,125 @@ import com.falah.models.Error;
 import com.falah.models.*;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-import okhttp3.*;
-import org.jetbrains.annotations.NotNull;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.lang.reflect.Type;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
 
 public class RampService {
 
-    public static String baseUrl = "https://progress-api-bexy.digital-logic-gen.com/api/";
+    public static String baseUrl = "https://api-bexy.digital-logic-gen.com/api/";
 
-    private OkHttpClient client = new OkHttpClient();
     private Gson gson = new Gson();
+    private final Executor executor = Executors.newSingleThreadExecutor();
+
+    private void sendRequest(String url, String method, String requestBody, OnCompleteListener<String> listener) {
+        executor.execute(() -> {
+            try {
+                URL urlObj = new URL(url);
+                HttpURLConnection connection = (HttpURLConnection) urlObj.openConnection();
+                connection.setRequestMethod(method);
+                connection.setRequestProperty("Content-Type", "application/json");
+                connection.setRequestProperty("Accept", "application/json");
+
+                if (requestBody != null) {
+                    connection.setDoOutput(true);
+                    try (OutputStream os = connection.getOutputStream()) {
+                        byte[] input = requestBody.getBytes(StandardCharsets.UTF_8);
+                        os.write(input, 0, input.length);
+                    }
+                }
+
+                int responseCode = connection.getResponseCode();
+
+                if (responseCode >= 200 && responseCode < 300) {
+                    try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8))) {
+                        StringBuilder response = new StringBuilder();
+                        String line;
+                        while ((line = reader.readLine()) != null) {
+                            response.append(line);
+                        }
+                        if (listener != null) {
+                            if (responseCode == 204) {
+                                listener.onComplete(null, null);
+                            } else {
+                                if (method.equals("POST") && url.contains("Reservations/TruckTrips/Reserve")) {
+                                    String contentDisposition = connection.getHeaderField("Content-Disposition");
+                                    String[] parts = contentDisposition.split("=");
+                                    String fileName = parts[1];
+                                    fileName = fileName.split("\\.")[0];
+                                    listener.onComplete(fileName, null);
+                                } else {
+                                    listener.onComplete(response.toString(), null);
+                                }
+                            }
+
+                        }
+                    }
+                } else {
+                    try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getErrorStream(), StandardCharsets.UTF_8))) {
+                        StringBuilder response = new StringBuilder();
+                        String line;
+                        while ((line = reader.readLine()) != null) {
+                            response.append(line);
+                        }
+                        if (listener != null) {
+                            listener.onComplete(null, gson.fromJson(response.toString(), Error.class));
+                        }
+                    }
+                }
+                connection.disconnect();
+            } catch (IOException e) {
+                if (listener != null) {
+                    listener.onComplete(null, new Error(e.getMessage()));
+                }
+            }
+        });
+    }
 
     public void getRamps(OnListCompleteListener<Ramp> listener) {
-        Request request = new Request.Builder().url(baseUrl + "Identity/User/Zone/Ramps?ageSize=15").build();
-
-        client.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(okhttp3.Call call, IOException e) {
-                e.printStackTrace();
+        sendRequest(baseUrl + "Identity/User/Zone/Ramps?ageSize=15", "GET", null, (response, error) -> {
+            if (error != null) {
+                listener.onComplete(null, error);
+                return;
             }
-
-            @Override
-            public void onResponse(okhttp3.Call call, okhttp3.Response response) throws IOException {
-
-                var body = response.body().string();
-                if (!response.isSuccessful()) {
-                    var error = gson.fromJson(body, Error.class);
-                    listener.onComplete(null, error);
-                    return;
-                }
-                JSONObject jsonObject = null;
-                try {
-                    jsonObject = new JSONObject(body);
-                    JSONArray list = jsonObject.getJSONArray("ramps");
-                    List<Ramp> ramps = Arrays.stream(gson.fromJson(list.toString(), Ramp[].class)).collect(Collectors.toList());
-                    listener.onComplete(ramps, null);
-                } catch (JSONException e) {
-
-                }
+            try {
+                JSONObject jsonObject = new JSONObject(response);
+                JSONArray list = jsonObject.getJSONArray("ramps");
+                List<Ramp> ramps = Arrays.stream(gson.fromJson(list.toString(), Ramp[].class)).collect(Collectors.toList());
+                listener.onComplete(ramps, null);
+            } catch (JSONException e) {
+                listener.onComplete(null, new Error(e.getMessage()));
             }
         });
     }
 
     public void getRamp(int id, OnCompleteListener<RampDetails> listener) {
-        Request request = new Request.Builder().url(baseUrl + "/Operations/Operations/active/" + id).build();
-
-        client.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(okhttp3.Call call, IOException e) {
-                listener.onComplete(null, new Error(e.getMessage()));
+        sendRequest(baseUrl + "/Operations/Operations/active/" + id, "GET", null, (response, error) -> {
+            if (error != null) {
+                listener.onComplete(null, error);
+                return;
             }
-
-            @Override
-            public void onResponse(okhttp3.Call call, okhttp3.Response response) throws IOException {
-                String body = response.body().string();
-                if (!response.isSuccessful()) {
-                    var error = gson.fromJson(body, Error.class);
-                    listener.onComplete(null, error);
-                    return;
-                }
-                RampDetails ramp = gson.fromJson(body, RampDetails.class);
-                listener.onComplete(ramp, null);
-            }
+            RampDetails ramp = gson.fromJson(response, RampDetails.class);
+            listener.onComplete(ramp, null);
         });
     }
 
     public void doRampAction(int id, RampDetails.ACTION action, UUID oprationsId, int cargoId, boolean callAnotherPickupTrip, OnCompleteListener<RampDetails> listener) {
-
         String url = switch (action) {
             case READY -> baseUrl + "Core/Ramps/" + id + "/Ready";
             case CHECKING -> baseUrl + "Operations/Operations/" + oprationsId + "/checking";
@@ -92,162 +135,57 @@ public class RampService {
             case STOP -> baseUrl + "Operations/Operations/" + oprationsId + "/stop";
         };
 
-        Request request = new Request.Builder().url(url).post(RequestBody.create("", null)).build();
-
-        client.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(okhttp3.Call call, IOException e) {
-                listener.onComplete(null, new Error(e.getMessage()));
+        sendRequest(url, "POST", "", (response, error) -> {
+            if (error != null) {
+                listener.onComplete(null, error);
+                return;
             }
-
-            @Override
-            public void onResponse(okhttp3.Call call, okhttp3.Response response) throws IOException {
-                String body = response.body().string();
-                if (!response.isSuccessful()) {
-                    var error = gson.fromJson(body, Error.class);
-                    listener.onComplete(null, error);
-                    return;
-                }
-                RampDetails ramp = gson.fromJson(body, RampDetails.class);
-                listener.onComplete(ramp, null);
-            }
+            RampDetails ramp = gson.fromJson(response, RampDetails.class);
+            listener.onComplete(ramp, null);
         });
     }
 
     public void checkReservation(String plateNumber, OnCompleteListener<String> listener) {
-        var url = baseUrl + "Reservations/TruckTrips?PlateNumber=" + plateNumber;
-
-        Request request = new Request.Builder().url(url).get().build();
-
-        client.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(@NotNull okhttp3.Call call, @NotNull IOException e) {
-                listener.onComplete(null, new Error(e.getMessage()));
+        sendRequest(baseUrl + "Reservations/TruckTrips?PlateNumber=" + plateNumber, "GET", null, (response, error) -> {
+            if (error != null) {
+                listener.onComplete(null, error);
+                return;
             }
+            Type pageType = new TypeToken<Page<TruckTrip>>() {
+            }.getType();
+            Page<TruckTrip> page = gson.fromJson(response, pageType);
 
-            @Override
-            public void onResponse(@NotNull okhttp3.Call call, @NotNull okhttp3.Response response) throws IOException {
-                String body = response.body().string();
+            List<TruckTrip> items = page.getItems();
 
-                if (!response.isSuccessful()) {
-                    var error = gson.fromJson(body, Error.class);
-                    listener.onComplete(null, error);
-                    return;
-                }
-//                Page page = gson.fromJson(body, Page.class);
-                Type pageType = new TypeToken<Page<TruckTrip>>() {
-                }.getType();
-                Gson gson = new Gson();
-                Page<TruckTrip> page = gson.fromJson(body, pageType);
-
-                // Access the items and other properties of the Page object
-                List<TruckTrip> items = page.getItems();
-
-
-                if (page.getRowCount() > 0 && items.get(0).getStatus() == 0) {
-                    listener.onComplete(items.get(0).getId(), null);
+            if (page.getRowCount() > 0 && items.get(0).getStatus() == 0) {
+                listener.onComplete(items.get(0).getId(), null);
+            } else {
+                var err = new Error();
+                if (page.getRowCount() > 0 && (items.get(0).getStatus() != 2 && items.get(0).getStatus() != 3)) {
+                    err.setDetail("هذه الشاحنة لا تزال قيد المعالجة");
+                    err.setStatus(400);
                 } else {
-                    var error = new Error();
-                    if (page.getRowCount() > 0 && (items.get(0).getStatus() != 2 && items.get(0).getStatus() != 3)) {
-                        error.setDetail("هذه الشاحنة لا تزال قيد المعالجة");
-                        error.setStatus(400);
-                    } else {
-                        error.setDetail("This truck is not reserved");
-                        error.setStatus(200);
-                    }
-                    listener.onComplete(null, error);
-
+                    err.setDetail("This truck is not reserved");
+                    err.setStatus(200);
                 }
-
+                listener.onComplete(null, err);
             }
         });
-
     }
 
     public void reserve(String plateNumber, OnCompleteListener<String> listener) {
-        var url = baseUrl + "Reservations/TruckTrips/Reserve";
-
-        Request request = new Request.Builder().url(url).post(RequestBody.create("{\"plateNumber\":\"" + plateNumber + "\"}", MediaType.get("application/json"))).build();
-
-        client.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(@NotNull okhttp3.Call call, @NotNull IOException e) {
-                listener.onComplete(null, new Error(e.getMessage()));
-            }
-
-            @Override
-            public void onResponse(@NotNull okhttp3.Call call, @NotNull okhttp3.Response response) throws IOException {
-                String body = response.body().string();
-                if (!response.isSuccessful()) {
-                    var error = gson.fromJson(body, Error.class);
-                    listener.onComplete(null, error);
-                    return;
-                }
-                Headers headers = response.headers();
-                String contentDisposition = headers.get("Content-Disposition");
-                String[] parts = contentDisposition.split("=");
-                String fileName = parts[1];
-                fileName = fileName.split("\\.")[0];
-                listener.onComplete(fileName, null);
-            }
-        });
-
+        String requestBody = "{\"plateNumber\":\"" + plateNumber + "\"}";
+        sendRequest(baseUrl + "Reservations/TruckTrips/Reserve", "POST", requestBody, listener);
     }
 
     public void enterBexy(String tripId, OnCompleteListener<Boolean> listener) {
-        var url = baseUrl + "Reservations/Gates/Enter-Bexy";
-
-        var requestBody = "{" + "\"tripId\":\"" + tripId + "\"," + "\"vehicleType\": 0" + "}";
-
-        Request request = new Request.Builder().url(url).put(RequestBody.create(requestBody, MediaType.get("application/json"))).build();
-
-        client.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(@NotNull okhttp3.Call call, @NotNull IOException e) {
-                e.printStackTrace();
-                listener.onComplete(null, new Error(e.getMessage()));
+        String requestBody = "{" + "\"tripId\":\"" + tripId + "\"," + "\"vehicleType\": 0 + \"}";
+        sendRequest(baseUrl + "Reservations/Gates/Enter-Bexy", "PUT", requestBody, (response, error) -> {
+            if (error != null) {
+                listener.onComplete(null, error);
+                return;
             }
-
-            @Override
-            public void onResponse(@NotNull okhttp3.Call call, @NotNull okhttp3.Response response) throws IOException {
-                String body = response.body().string();
-                if (!response.isSuccessful()) {
-                    var error = gson.fromJson(body, Error.class);
-                    listener.onComplete(null, error);
-                }
-                listener.onComplete(true, null);
-            }
+            listener.onComplete(true, null);
         });
     }
-
-//    public void enterWaitingYard(String tripId, OnCompleteListener<Boolean> listener) {
-//        var url = baseUrl + "Reservations/Gates/Enter-Waiting-Yard";
-//        var requestBody = "{" +
-//                          "\"tripId\":\"" + tripId + "\"," +
-//                          "\"vehicleType\": 0" +
-//                          "}";
-//        Request request = new Request.Builder().url(url).
-//                put(RequestBody.create(requestBody, MediaType.get("application/json"))).build();
-//
-//        client.newCall(request).enqueue(new Callback() {
-//            @Override
-//            public void onFailure(@NotNull okhttp3.Call call, @NotNull IOException e) {
-//                e.printStackTrace();
-//                listener.onComplete(null, new Error(e.getMessage()));
-//            }
-//
-//            @Override
-//            public void onResponse(@NotNull okhttp3.Call call, @NotNull okhttp3.Response response) throws IOException {
-//                String body = response.body().string();
-//                if (!response.isSuccessful()) {
-//                    var error = gson.fromJson(body, Error.class);
-//                    listener.onComplete(null, error);
-//                }
-//                listener.onComplete(true, null);
-//            }
-//        });
-//
-//
-//    }
-
 }
